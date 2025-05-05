c6dc8b990dbda30f694a52ce6310de20ab6933d1
Shawn Mathew <shawn.mathew@maryland.gov>
2025-05-05 15:06:32 -0400

    use nova model

--- amplify/data/bedrock.js
+++ amplify/data/bedrock.js
@@ -1,3 +1,53 @@
+const claudeRequest = {
+  resourcePath: `/model/anthropic.claude-3-7-sonnet-20250219-v1:0/invoke`,    
+  method: 'POST',
+  params: {
+    headers: {
+      'Content-Type': 'application/json'
+    },    
+    body: JSON.stringify({        
+      anthropic_version: 'bedrock-2023-05-31',
+      max_tokens: 200,
+      top_k: 250,
+      stop_sequences: [],
+      temperature: 1,
+      top_p: 0.999,
+      messages: [
+        {
+          role: 'user',
+          content: [
+            {
+              type: 'text',
+              text: 'Why is the sky blue?'
+            }
+          ]
+        }
+      ]
+    })
+  },
+}
+
+const novaRequest = {
+  "modelId": "amazon.nova-lite-v1:0",
+  "contentType": "application/json",
+  "accept": "application/json",
+  "body": {
+    "inferenceConfig": {
+      "max_new_tokens": 1000
+    },
+    "messages": [
+      {
+        "role": "user",
+        "content": [
+          {
+            "text": "Why is the sky blue?"
+          }
+        ]
+      }
+    ]
+  }
+}
+
 export function request(ctx) {
   const { ingredients = [] } = ctx
 
@@ -5,34 +55,7 @@ export function request(ctx) {
   const prompt = `Suggest a recipe idea using these ingredients ${ingredients.join(', ')}`
 
   // Return the request config
-  return {
-    resourcePath: `/model/anthropic.claude-3-7-sonnet-20250219-v1:0/invoke`,
-    method: 'POST',
-    params: {
-      headers: {
-        'Content-Type': 'application/json'
-      },    
-      body: JSON.stringify({        
-        anthropic_version: 'bedrock-2023-05-31',
-        max_tokens: 200,
-        top_k: 250,
-        stop_sequences: [],
-        temperature: 1,
-        top_p: 0.999,
-        messages: [
-          {
-            role: 'user',
-            content: [
-              {
-                type: 'text',
-                text: 'Why is the sky blue?'
-              }
-            ]
-          }
-        ]
-      })
-    },
-  }
+  return novaRequest
 }
 
 export function response(ctx) {
