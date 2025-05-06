b01a3a7d472480e71c146a1a896f3777908665a2
Shawn Mathew <shawn.mathew@maryland.gov>
2025-05-06 14:05:39 -0400

    return request obj directly in lambda request

--- amplify/data/bedrock.js
+++ amplify/data/bedrock.js
@@ -34,7 +34,30 @@ export function request(ctx) {
 
   // Return the request config
   // return novaRequest(prompt)
-  return ctx
+  return {
+    resourcePath: `/model/amazon.nova-lite-v1:0/invoke`,
+    method: 'POST',  
+    params: {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "inferenceConfig": {
+          "max_new_tokens": 1000
+        },
+        "messages": [
+          {
+            "role": "user",
+            "content": [
+              { 
+                text: prompt
+              }
+            ]
+          }
+        ]
+      })
+    }  
+  }
 }
 
 export function response(ctx) {
@@ -46,8 +69,8 @@ export function response(ctx) {
     body: parsedBody.output.message.content[0].text
   }
 
-  // return res
-  return ctx
+  return res
+  // return ctx
 }
 
 // export const handler = async (event) => {
